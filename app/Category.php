b21e97ad99c50d23fd4976ee90c7843292b1e3d0
AmirMasoud Sheydaei <amirmasoud.sheydaei@gmail.com>
2018-08-04 18:09:57 +0430

    🕳 Add pivot as hidden

--- app/Category.php
+++ app/Category.php
@@ -18,6 +18,15 @@ class Category extends Model
     ];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'pivot',
+    ];
+
+    /**
      * A category belongs to an icon.
      *
      * @return \App\Icon
