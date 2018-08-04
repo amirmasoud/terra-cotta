e2b8397c813354b0b92ce884fdb4c5d799b013f0
AmirMasoud Sheydaei <amirmasoud.sheydaei@gmail.com>
2018-08-04 18:14:05 +0430

    🌧 Add loading to card

--- resources/assets/js/components/Card.vue
+++ resources/assets/js/components/Card.vue
@@ -1,13 +1,20 @@
 <template>
-  <b-card :header="title ? title : ''"
-    class="mt-2">
-    <slot/>
-    <template v-if="routerName && routerLink">
-      <b-link
-        :to="{ name: routerLink }"
-        class="card-link">
-      {{ routerName }}
-      </b-link>
+  <b-card :header="title ? title : ''" class="mt-2">
+    <template v-if="loading">
+      <div class="text-center p-3">
+        <fa icon="spinner" size="lg" fixed-width spin />
+        <p class="mb-0 mt-1" v-if="loadingMessage">{{ loadingMessage }}</p>
+      </div>
+    </template>
+    <template v-else>
+      <slot/>
+      <template v-if="routerName && routerLink">
+        <b-link
+          :to="{ name: routerLink }"
+          class="card-link">
+        {{ routerName }}
+        </b-link>
+      </template>
     </template>
   </b-card>
 </template>
@@ -30,6 +37,16 @@ export default {
     routerLink: {
       type: String,
       default: '#'
+    },
+
+    loading: {
+      type: Boolean,
+      default: false
+    },
+
+    loadingMessage: {
+      type: String,
+      default: null
     }
   },
 }
