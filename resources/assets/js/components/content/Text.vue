7241cad5ec69b9bdb43acd89c8731e69961170fb
AmirMasoud Sheydaei <amirmasoud.sheydaei@gmail.com>
2018-08-23 17:57:23 +0430

    Fix #36

--- resources/assets/js/components/content/Text.vue
+++ resources/assets/js/components/content/Text.vue
@@ -3,7 +3,7 @@
   <div class="form-group row">
     <label class="col-md-3 col-form-label text-md-right"><slot></slot></label>
     <div class="col-md-7">
-      <input v-model="form[name]" :class="{ 'is-invalid': form.errors.has(name) }" class="form-control" type="text" :name="name">
+      <input v-model="form[name]" :class="{ 'is-invalid': form.errors.has(name) }" class="form-control" type="text" :name="name" @focus="$event.target.select()">
       <has-error :form="form" :field="name"/>
     </div>
   </div>
