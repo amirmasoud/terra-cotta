eb518df1aee19627442ed04a7230609c35279528
Amirmasoud <amirmasood33@gmail.com>
2020-04-12 14:45:47 +0430

    Update...

---
+++ layout/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="antialiased text-gray-900">
+    <div class="bg-gray-200">
+      <Navbar/>
+    </div>
+    <div class="container mx-auto">
+      <div class="flex flex-wrap">
+        <PropertyCard class="w-full sm:w-1/2 lg:w-1/3 p-4" :property="property"/>
+        <PropertyCard class="w-full sm:w-1/2 lg:w-1/3 p-4" :property="property"/>
+        <PropertyCard class="w-full sm:w-1/2 lg:w-1/3 p-4" :property="property"/>
+        <PropertyCard class="w-full sm:w-1/2 lg:w-1/3 p-4" :property="property"/>
+        <PropertyCard class="w-full sm:w-1/2 lg:w-1/3 p-4" :property="property"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import PropertyCard from './components/PropertyCard'
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    PropertyCard
+  },
+  data: () => ({
+    property: {
+      imageUrl: 'img/colorado.jpg',
+      imageAlt: 'Colorado',
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center",
+      priceCents: "190000",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4
+    }
+  })
+}
+</script>
+
+<style src="./assets/tailwind.css"></style>
---
+++ layout/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+    <div class="flex items-center justify-between px-4 py-3 sm:p-0">
+      <div>
+        <img class="h-8" src="/img/logo-inverted.svg" alt="Workcation">
+      </div>
+      <div class="sm:hidden">
+        <button @click="isOpen = !isOpen" type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+            <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+    <nav :class="isOpen ? 'block' : 'hidden'" class="sm:block">
+      <div class="px-2 pt-2 pb-4 sm:flex sm:p-0">
+        <a href="#" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">List your property</a>
+        <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Trips</a>
+        <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Messages</a>
+        <AccountDropdown class="hidden sm:block sm:ml-6"/>
+      </div>
+      <div class="px-4 py-5 border-t border-gray-800 sm:hidden">
+        <div class="flex items-center">
+          <img class="h-8 w-8 border-2 border-gray-600 rounded-full object-cover" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar">
+          <span class="ml-3 font-semibold text-white">Jane Doe</span>
+        </div>
+        <div class="mt-4">
+          <a href="#" class="block text-gray-400 hover:text-white">Account settings</a>
+          <a href="#" class="mt-2 block text-gray-400 hover:text-white">Support</a>
+          <a href="#" class="mt-2 block text-gray-400 hover:text-white">Sign out</a>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import AccountDropdown from './AccountDropdown'
+export default {
+  components: {
+    AccountDropdown,
+  },
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+}
+</script>
