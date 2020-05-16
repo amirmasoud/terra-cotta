d43fb7a29cf5f330816520fa41c69b818134ac78
Amirmasoud <amirmasood33@gmail.com>
2020-05-16 11:08:02 +0430

    password.reset API

--- backend/app/Http/Controllers/Auth/ResetPasswordController.php
+++ backend/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +32,38 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function reset(Request $request)
+    {
+        $validator = Validator::make($request->all(), $this->rules());
+        if ($validator->fails()) {
+            return new JsonResponse($validator->errors(), 400);
+        }
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request),
+            function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        if ($response != Password::PASSWORD_RESET) {
+            throw ValidationException::withMessages([
+                'email' => [trans($response)],
+            ]);
+        }
+        return new JsonResponse(['message' => trans($response)], 200);
+    }
 }
