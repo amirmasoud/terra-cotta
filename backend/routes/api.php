b7fbe03363d50d40aef33a083a50446f517c5ea1
Amirmasoud <amirmasood33@gmail.com>
2020-05-16 12:21:03 +0430

    Resource INDEX

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/crud/{resource}', 'Resource\IndexController@index');
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
     Route::get('/user', 'UserController@user');
     Route::get('/home', 'HomeController@get');
+
 
     Route::get('settings/users/search', 'Settings\UserController@search')
         ->name('users.search');
