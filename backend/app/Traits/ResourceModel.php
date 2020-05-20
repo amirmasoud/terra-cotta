b4bf376048b43f2c11ca7782114906b6e8d26b9e
Amirmasoud <amirmasood33@gmail.com>
2020-05-20 12:03:07 +0430

    Fix

--- backend/app/Traits/ResourceModel.php
+++ backend/app/Traits/ResourceModel.php
@@ -17,7 +17,6 @@ trait ResourceModel
     /**
      * Return model based on route resource.
      *
-<<<<<<< HEAD
      * @return Illuminate\Database\Eloquent\Model|null
      */
     public function __construct()
@@ -51,38 +50,5 @@ trait ResourceModel
             }
         }
         return $relations;
-=======
-     * @return Illuminate\Database\Eloquent\Model
-     */
-    public static function model()
-    {
-        return $model ?? config('terracotta.model.namespace') . Str::of(Route::current()->resource)->singular()->title();
-    }
-
-    /**
-     * Invoke method on the model.
-     *
-     * @param string $name
-     * @param array $arguments
-     * @return mixed
-     */
-    public static function __callStatic($name, $arguments)
-    {
-        if (empty($arguments)) {
-            return [self::model(), $name]();
-        } else {
-            return [self::model(), $name]($arguments);
-        }
-    }
-
-    /**
-     * Return model name.
-     *
-     * @return string
-     */
-    public function __toString()
-    {
-        return self::model();
->>>>>>> 889ec9e29754be4bc495df809ea95687c87da754
     }
 }
