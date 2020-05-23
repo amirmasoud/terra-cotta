838e02bdd343f8141238fd1a70ed5dc05241178c
Amirmasoud <amirmasood33@gmail.com>
2020-05-23 12:10:34 +0430

    WIP

--- layout/src/components/Modal.vue
+++ layout/src/components/Modal.vue
@@ -111,7 +111,6 @@
             >
               <div class="px-2 pt-1 pb-2 mt-1 text-indigo-100">Save</div>
             </button>
-            {{ form }}
           </div>
         </div>
         <slot />
@@ -150,6 +149,8 @@ export default {
     },
   }),
   mounted() {
+    axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {})
+    console.log(document.cookie)
     for (let resource of ['tags', 'categories']) {
       axios
         .get('http://localhost:8000/api/admin/' + resource)
